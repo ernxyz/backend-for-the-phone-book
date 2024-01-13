fab939b8ad1aa7980b7a2dcec6ce104955483385
ernxyz <ernej29@gmail.com>
2024-01-12 22:37:11 -0500

    link added

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # part3-full-stack-open
+
+[link to backend app](https://backend-for-the-phone-book.onrender.com/)
--- index.js
+++ index.js
@@ -124,7 +124,7 @@ const unknownEndpoint = (req, res) => {
 
 app.use(unknownEndpoint)
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`server listening on port ${PORT}`);
 })
